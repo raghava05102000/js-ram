3bd65530e9339747ddcd8bc08578f9ab8f154d26
Raghava H D <136361442+raghava05102000@users.noreply.github.com>
2024-01-08 13:40:40 +0000

    commit config done

---
+++ javascript/01_index.js
@@ -0,0 +1,7 @@
+console.log(null); //null
+console.log(typeof (null)); //object
+console.log(undefined); //undefined
+console.log(typeof (undefined)) //undfined
+console.log(NaN) //Nan
+console.log(typeof (NaN))// number
+console.log(typeof Object)// function
--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,5 @@
 var city = "Mysore"
 let email = "arya@gmail.com"
 const accountNumber = 123456789
-
 console.log([city, email, accountNumber])
 console.table([city, email, accountNumber])
