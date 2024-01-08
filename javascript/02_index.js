1ad5d5695f55811a06e221ec43bafcba37135bdb
Raghava H D <136361442+raghava05102000@users.noreply.github.com>
2024-01-08 13:54:30 +0000

    converstion of datatypes

---
+++ javascript/02_index.js
@@ -0,0 +1,23 @@
+// let score = "05qw";
+
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(valueInNumber)
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn) // true
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)// false
+
+// let isLoggedIn = "ARYA"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)// true
+
+let randomNumber = 33
+let stringNumber = String(randomNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber) //string
