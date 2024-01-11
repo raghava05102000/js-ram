343c926cd4e29a1606f3e7546deeb8f47cb81fbe
Raghava H D <136361442+raghava05102000@users.noreply.github.com>
2024-01-11 07:21:58 +0000

    array methods

---
+++ javascript/08_array.js
@@ -0,0 +1,28 @@
+let myArr = [0, 1, 2, 3, 4, 5, 6, 7]
+// console.log(myArr[0]); // 0
+// console.log(myArr[8]); // undefined
+
+//Array Methods
+// myArr.push(8);
+// myArr.push(9)
+// myArr.pop()
+
+// myArr.unshift(-1)
+// myArr.shift()
+
+// console.log(myArr.includes(0));
+// console.log(myArr.indexOf(7));
+
+// const newArr = myArr.join()
+// console.log(newArr);
+// console.log(myArr);
+
+
+//Slice, Splice
+console.log("1", myArr);
+
+let myArr1 = myArr.slice(1, 5);
+console.log("2", myArr1);
+
+let myArr2 = myArr.splice(1, 5);
+console.log("3", myArr2);
