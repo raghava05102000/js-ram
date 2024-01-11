3db5e582b16a39d3ef57c37ded6166f0ac2afbf0
Raghava H D <136361442+raghava05102000@users.noreply.github.com>
2024-01-11 09:30:22 +0000

    array another methods

--- javascript/08_array.js
+++ javascript/08_array.js
@@ -19,10 +19,36 @@ let myArr = [0, 1, 2, 3, 4, 5, 6, 7]
 
 
 //Slice, Splice
-console.log("1", myArr);
+// console.log("1", myArr);
 
-let myArr1 = myArr.slice(1, 5);
-console.log("2", myArr1);
+// let myArr1 = myArr.slice(1, 5);
+// console.log("2", myArr1);
 
-let myArr2 = myArr.splice(1, 5);
-console.log("3", myArr2);
+// let myArr2 = myArr.splice(1, 5);
+// console.log("3", myArr2);
+
+let mysorePlaces = ["Chamundi Hills", "Palace", "Zoo", "Nanjangud Temple"]
+let karnatakaPlaces = ["Mangaluru Beachs", "Mysore", "Bangaluru", "ChikkaMangaluru"]
+
+// mysorePlaces.push(karnatakaPlaces);
+
+let allPlaces = mysorePlaces.concat(karnatakaPlaces)
+
+// console.log(allPlaces);
+
+let all_places_of_karnataka = [...mysorePlaces, ...karnatakaPlaces]
+// console.log(typeof all_places_of_karnataka);
+
+let anotherArray = [0,1,2,3,4,5,["Chamundi Hills", "Palace", "Zoo", "Nanjangud Temple",["Mangaluru Beachs", "Mysore", "Bangaluru", "ChikkaMangaluru"]]]
+
+let real_another_Array = anotherArray.flat(Infinity)
+// console.log(real_another_Array);
+
+console.log(Array.isArray("Raghava"))
+console.log(Array.from("Raghava"));
+console.log(Array.from({name: "raghava"}));
+
+let name = "arya";
+let age = 23;
+let email = "arya@github.com"
+console.log(Array.of(name, age, email));
